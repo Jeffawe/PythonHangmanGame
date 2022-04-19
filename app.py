0a88a01cdcf551421d1a3b30f2ef9217fc9460e0
Jeffawe <awagujeffery@gmail.com>
2022-04-19 10:47:33 +0100

    Game Fully Working

--- app.py
+++ app.py
@@ -13,11 +13,10 @@ from flask_session import Session
 import time
 
 app = Flask(__name__)
-app.secret_key = b'\x8c\xa1\xb2\xb1\x16\x0c1b\x0e\xfd|1\xef\x10\x92,~\x11>\xa8\xa5\x9c$\x05'
+app.config['secret_key'] = b'\x8c\xa1\xb2\xb1\x16\x0c1b\x0e\xfd|1\xef\x10\x92,~\x11>\xa8\xa5\x9c$\x05'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
 
 
 def score_control():
@@ -348,3 +347,5 @@ def games_post():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
